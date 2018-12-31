7950a5506bb9d6fcde492845f39fad4f57f9fcf6
Philip Winnington <ftcpjw@yahoo.com>
2018-12-31 17:07:28 -0500

    updated posts to display all posts and updated rspec as well

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "creation" do
     before do
-     @post = Post.create(date: Date.today, rationale: "anything")
+     user = User.create(email: 'testpost@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jon', last_name: 'snow')
+     @post = Post.create(date: Date.today, rationale: "anything", user_id: user.id)
     end
     it "can be created" do
       expect(@post).to be_valid
