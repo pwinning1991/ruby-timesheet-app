584424c2e3fd549006382a75b21bbb3f1c9e343b
Philip Winnington <ftcpjw@yahoo.com>
2019-01-06 23:08:03 -0500

    implemented tests and page redirection for permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 end
