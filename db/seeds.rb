a2ac3708fc44a6a25676858975c102f7face3e65
Philip Winnington <ftcpjw@yahoo.com>
2019-01-06 21:04:03 -0500

    updated seeds file and Admin dashboard to show status of post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 @user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jon', last_name: 'Snow')
 
 puts "1 user created"
+
+AdminUser.create(email: 'admin@admin.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Admin', last_name: 'User')
+puts "1 admin user created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
