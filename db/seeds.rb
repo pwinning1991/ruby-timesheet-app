106de52c2f48116cd0ec3a4f4bf646a48c1c48fd
Philip Winnington <ftcpjw@yahoo.com>
2019-01-08 22:45:26 -0500

    updated factory girl for audit log

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
                     password_confirmation: 'asdfasdf', 
                     first_name: 'Jon', 
                     last_name: 'Snow',
-                    phone: "123123123" ) 
+                    phone: "1234567890" ) 
 
 puts "1 user created"
 
@@ -12,7 +12,7 @@ AdminUser.create(email: 'admin@admin.com',
                  password_confirmation: 'asdfasdf', 
                  first_name: 'Admin', 
                  last_name: 'User',
-                 phone: "12312334")
+                 phone: "1234567890")
 
 puts "1 admin user created"
 
@@ -21,3 +21,8 @@ puts "1 admin user created"
 end
 
 puts "100 posts have been created"
+
+100.times do |log|
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+puts "100 Audit logs  have been created"
