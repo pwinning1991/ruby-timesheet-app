cdee62e340aec4daadcfc8fdd634c67c22e2b248
Philip Winnington <ftcpjw@yahoo.com>
2019-01-09 21:55:08 -0500

    integrated tests fro autdit log

--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @audit_log = FactoryGirl.create(:audit_log)
+  end
+  describe 'creation' do
+    it 'can be properly created' do
+      expect(@audit_log).to be_valid
+    end
+  end
+
+  describe 'validations' do
+    it 'it should be required to have a user assoiation' do
+      @audit_log.user_id = nil
+      expect(@audit_log).to_not be_valid
+    end
+    it 'it should always have a staus' do
+      @audit_log.status = nil
+      expect(@audit_log).to_not be_valid
+    end
+    it 'it should be required to have a start date' do
+      @audit_log.start_date = nil
+      expect(@audit_log).to_not be_valid
+    end
+    it 'it should have a start date equal to 6 days prior' do
+      new_audit_log = AuditLog.create!(user_id: User.last.id, status: 0 )
+      expect(new_audit_log.start_date).to eq(Date.today - 6.days)
+    end
+  end
 end
