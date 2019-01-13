c1324f269e3ddade703c6566c01266f3758b46cb
Philip Winnington <ftcpjw@yahoo.com>
2019-01-13 12:31:31 -0500

    styled the employee homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,8 @@ end
 
 puts "100 posts have been created"
 
-100.times do |log|
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-end
-puts "100 Audit logs  have been created"
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created"
