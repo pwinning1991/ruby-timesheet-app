ef8ab018188c93e2b4d7a824b934e86030beef1a
Philip Winnington <ftcpjw@yahoo.com>
2019-01-13 17:58:36 -0500

    upadted seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,11 @@ AdminUser.create(email: 'admin@admin.com',
                  phone: "1234567890")
 
 puts "1 admin user created"
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",  user_id: @user.id, overtime_request: 2.5)
@@ -22,8 +27,4 @@ end
 
 puts "100 posts have been created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
-puts "3 audit logs have been created"
