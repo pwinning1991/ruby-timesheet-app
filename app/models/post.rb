ab2bed183c33390b532ada3afaff89e225c271ab
Philip Winnington <ftcpjw@yahoo.com>
2019-01-13 20:21:22 -0500

    implemented nil gaurd for call back on audit logs

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,6 @@ class Post < ApplicationRecord
   private
   def update_audit_log
     audit_log = AuditLog.where(user_id: self.user.id, start_date: (self.date - 7.days..self.date)).last
-    audit_log.confirmed!
+    audit_log.confirmed! if audit_log
   end
 end
